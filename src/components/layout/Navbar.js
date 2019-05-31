2af359d57692286c55596442cf24c8d2d446720c
ShanKwanCho <scho4@mail.sfsu.edu>
2019-05-31 16:26:38 -0700

    self-develop-react

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+ import React from 'react';
+ 
+ const Navbar=() => {
+     return (
+     <nav className = "navbar navbar-dark bg-dark variant-dark mb-5" fixed="top">
+         <span className = "navbar-brand mb-0 h1 mx-auto">Shan's Lyric Finder</span>
+     </nav>);
+ };
+ 
+
+ export default Navbar;  
